e403df65a8702dac6bf292bba21f2be9d079e58b
1plus <122733139+Chen1Plus@users.noreply.github.com>
2023-12-30 00:23:10 +0800

    0.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 mod player;
 
+use mki::{Action, Keyboard};
+use player::{SinkPool, SinkPoolError};
+use rand::prelude::*;
+use std::sync::Arc;
+
 fn main() {
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-    player::new(&stream_handle, "assets/knock_metal_door1.mp3");
+    let pool = Arc::new(SinkPool::new(&stream_handle, 20));
+
+    mki::bind_any_key(Action::handle_kb(move |key| {
+        if key == Keyboard::Escape {
+            std::process::exit(0);
+        }
+        let rng = thread_rng().gen_range(1..=6);
+        if let Err(SinkPoolError::NoFreeSink) =
+            pool.play_one(format!("assets/bad/{rng}.wav").as_str())
+        {
+            println!("no free sink");
+        }
+    }));
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(120));
+    }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,22 +1,56 @@
-use rodio::{Decoder, OutputStreamHandle, Sink};
-use std::{fs::File, io::BufReader};
+#![allow(dead_code)]
 
-pub fn new(stream_handle: &OutputStreamHandle, path: &str) -> Sink {
-    let sink = Sink::try_new(stream_handle).unwrap();
-    let source = Decoder::new(BufReader::new(
-        File::open(path).expect("failed to open file"),
-    ))
-    .unwrap_or_else(|_| panic!("failed to decode file: {}", path));
-    sink.append(source);
-    sink
+use rodio::{Decoder, OutputStreamHandle, Sink};
+use std::{
+    fs::File,
+    io::{self, BufReader},
+    iter,
+};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum SinkPoolError {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error("no sink available (all playing)")]
+    NoFreeSink,
+}
+
+pub struct SinkPool {
+    pub sinks: Vec<Sink>,
+}
+
+impl SinkPool {
+    pub fn new(stream_handle: &OutputStreamHandle, sink_cnt: usize) -> Self {
+        SinkPool {
+            sinks: iter::repeat_with(|| Sink::try_new(stream_handle).unwrap())
+                .take(sink_cnt)
+                .collect::<Vec<_>>(),
+        }
+    }
+
+    pub fn play_one(&self, path: &str) -> Result<(), SinkPoolError> {
+        match self.sinks.iter().find(|&sink| sink.empty()) {
+            None => Err(SinkPoolError::NoFreeSink),
+            Some(sink) => {
+                sink.append(Decoder::new(BufReader::new(File::open(path)?)).unwrap());
+                Ok(())
+            }
+        }
+    }
+
+    pub fn sleep_until_end(&self) {
+        self.sinks.iter().for_each(|sink| sink.sleep_until_end());
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn new_single() {
+    fn new_pool() {
         let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-        let a = super::new(&stream_handle, "assets/test/1.mp3");
-        a.sleep_until_end();
+        let pool = super::SinkPool::new(&stream_handle, 4);
+        pool.play_one("assets/test/1.mp3").unwrap();
+        pool.sleep_until_end();
     }
 }
